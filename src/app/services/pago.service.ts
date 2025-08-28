7f4c2e17966b81a4f68f361ba493ec78e697fe36
KnNgZmN <knngzmn@gmail.com>
2025-08-28 14:30:56 -0500

    Ajuste pago.service.ts

--- src/app/services/pago.service.ts
+++ src/app/services/pago.service.ts
@@ -12,7 +12,7 @@ export class PagoService {
   constructor(private http: HttpClient) { }
 
   crearOrden(monto: number) {
-    return this.http.post<any>(`${this.apiMenu}/paypal/createOrder`, { amount: monto });
+    return this.http.post<any>(`${this.apiMenu}/paypal`, { amount: monto });
   }
 
   capturarPago(
@@ -23,7 +23,7 @@ export class PagoService {
     producto: string,
     metodo: string
   ): Observable<any> {
-    return this.http.post<any>(`${this.apiMenu}/paypal/captureOrder`, {
+    return this.http.post<any>(`${this.apiMenu}/paypal`, {
       orderId,
       usuario,
       nombre,
